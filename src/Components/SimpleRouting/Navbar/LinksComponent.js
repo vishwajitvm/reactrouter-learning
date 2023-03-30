e29c0c0c71cccda5bab3382dda52c52ab9046020
vishwajitvm <vishwajitmall50@gmail.com>
2023-03-30 06:27:38 +0530

    React dynamic Routing with params

--- src/Components/SimpleRouting/Navbar/LinksComponent.js
+++ src/Components/SimpleRouting/Navbar/LinksComponent.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function LinksComponent() {
   return (
-    <div>
-      <Link to="/">Home</Link> <br />
-      <Link to="/simpleroute/home">Simple-router-Home</Link> <br />
-      <Link to="/simpleroute/about">Simple-router-About</Link>
+    <div style={{ backgroundColor: 'lightgreen' , padding:'15px 0px',textDecoration: 'none' }}>
+      <Link to="/" style={{textDecoration: 'none'}}>Home</Link> <br />
+      <Link to="/simpleroute/home" style={{textDecoration: 'none'}}>Simple-router-Home</Link> <br />
+      <Link to="/simpleroute/about" style={{textDecoration: 'none'}}>Simple-router-About</Link> <br />
+      <Link to="/simpleroute/users/vishwajit" style={{textDecoration: 'none'}}>Simple-routes-dynamic-users</Link> <br />
     </div>
   )
 }
--- src/Components/SimpleRouting/Navbar/NavbarComponent.js
+++ src/Components/SimpleRouting/Navbar/NavbarComponent.js
@@ -5,16 +5,18 @@ import Home from '../Home'
 import LinksComponent from './LinksComponent'
 import HomeMain from '../../HomeMain'
 import Page404 from '../../Page404'
+import UsersComponent from '../UsersDynamic/UsersComponent'
 
 export default function NavbarComponent() {
   return (
-    <div style={{ backgroundColor: 'lightblue' }}>
+    <div>
       <BrowserRouter>
       <LinksComponent />
       <Routes>
             <Route path="/" element={<HomeMain/>} />
             <Route path="/simpleroute/home" element={<Home/>} />
             <Route path="/simpleroute/about" element={<About/>} />
+            <Route path="/simpleroute/users/:name" element={<UsersComponent/>} />
             <Route path="/*" element={<Page404/>} />
         </Routes>
       </BrowserRouter>
