5a5722d19b81ca14a5971a1b48bd44b5f9ec3524
vishwajitvm <vishwajitmall50@gmail.com>
2023-03-30 22:06:16 +0530

    React useNavigate (on button click navigate to other pages or component)

--- src/Components/SimpleRouting/Home.js
+++ src/Components/SimpleRouting/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const navigate = useNavigate() ;
+  const navigateToAbout = () => {
+    navigate('/simpleroute/about')
+  }
+
+  const navigateToFilter = () => {
+    navigate('/simpleroute/filter')
+  }
+
   return (
     <div>
       <h1>Simple Routing Home page</h1>
+      <button onClick={()=>navigateToAbout()} >Go to About Page</button> <br />
+      <button onClick={()=>navigateToFilter()} >Go to Filter Page</button>
     </div>
   )
 }
